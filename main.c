72b2c5dd51fc5f0da1f08bc3f9b7d3b4724ee013
livingod <8350lyj2@gmail.com>
2023-11-05 23:58:01 +0900

    05(...)

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 
-void sqaure_array(int a[], int size)
+void addMatrix(int A[][COLS], int B[][COLS],int C[][COLS])
 {
 	int i;
-	
-	for (i=0;i<size;i++)
-		a[i] = a[i]*a[i];
+	for(i=0;i<)/////////////dmdkdkdkdkdkdkdkdk
+		C[][COLS] = A[][COLS] + B[][COLS];
 }
-
-void print_array(int a[], int size)
+void printMatirx(int A[][COLS])
 {
-	int i;
-	
-	for (i=0;i<size;i++)
-		printf("%3d", a[i]);
-	printf("\n");
+	///////////////dmdkdkdkdkdk
 }
-
-
 int main(int argc, char *argv[]) 
 {
-	int list[SIZE] = {1,2,3,4};
+	int A[ROWS][COLS] = {
+		{2,3,0}
+		{8,9,1}
+		{7,0,5}};
+	int B[ROWS][COLS] = {
+		{1,0,0}
+		{0,1,0}
+		{0,0,1}};	
+	int C[ROWS][COLS];
 	
-	print_array(list,SIZE);
-	square_array(list,SIZE);
-	print_array(list,SIZE);
-		
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
 	return 0;
 }
